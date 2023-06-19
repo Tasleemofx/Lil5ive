70b5db359419457bec48ee886a9933652c95272e
Tasleemofx <oladepotesleem5@gmail.com>
2023-06-19 05:13:39 +0100

    music player playing

---
+++ README.md
@@ -0,0 +1,3 @@
+LIL5IVE music website homepage
+
+hosted live on vercel[]
--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import { useState, useRef, useEffect } from 'react'
 import './App.css'
 import Nav from './components/Navbar/Nav'
 import Sidebar from './components/Sidebar/Sidebar'
@@ -14,9 +14,38 @@ import ellipse from "./assets/images/Ellipse.png"
 import ellipse2 from "./assets/images/Ellipse-4.png"
 import "./App.css"
 
+import { BsPauseCircle, BsPlayCircle } from 'react-icons/bs'
+
 function App() {
   const [opensidebar, setOpensidebar] = useState(false)
+  const [isplaying, setIsplaying] = useState(false)
+  const [progress, setProgress] = useState(0)
+  const [song, setSong] = useState(say)
 
+  const progressRef = useRef()
+  const songRef = useRef()
+  const controlRef = useRef()
+
+
+  useEffect(()=>{
+    setTimeout(()=>{
+      progress = song.value
+    },500)
+  },[songRef.current])
+  function getMusic(){
+    songRef.current.max = progress
+  
+    
+  }
+  function playPause(){
+    
+    if(!isplaying){
+      songRef.current.play()
+    }else{
+      songRef.current.pause()
+    }
+    setIsplaying(!isplaying)
+  }
   return (
     <div className="hpage-black">
       <Nav setOpensidebar={setOpensidebar} opensidebar={opensidebar}/>
@@ -33,14 +62,19 @@ function App() {
         <img src={playgrey} alt="playgrey" className="pgr"/>
       </div>
 
-      <img src={ellipse} alt="semi-circle" />
+      <img src={ellipse} alt="semi-circle" className="btm-circ"/>
       <img src={lil5text} alt="bgtxt" className="wmarktxt"/>
-      <img src={ellipse2} alt="semi-circle" />
-      <img src={amoeba} alt="amoeba" />
-      <audio controls>
-        <source src={say} type="audio/mp3" />
+      <img src={ellipse2} alt="semi-circle" className="btm-rgt"/>
+      <img src={amoeba} alt="amoeba" className="amoeba" />
+      <audio src={say} type="audio/mp3" ref={songRef} >
         Your browser cannot play this audio
       </audio>
+      <div className="fcol jsb-aic playern" >
+      {isplaying ?  <BsPauseCircle onClick={playPause}/>: <BsPlayCircle onClick={playPause}/> }
+
+      <input type="range" ref={progressRef} defaultValue={progress} 
+      onChange={(e)=> console.log(e.target.value)} className="audioprog"/>
+      </div>
     </div>
   )
 }
